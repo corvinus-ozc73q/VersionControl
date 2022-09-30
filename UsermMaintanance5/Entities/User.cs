b91d424b7c6ae47e33648e5a81de94f806bc0ba5
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2022-09-30 16:30:42 +0200

    Feature branch

--- UsermMaintanance5/Entities/User.cs
+++ UsermMaintanance5/Entities/User.cs
@@ -9,6 +9,7 @@ namespace UsermMaintanance5.Entities
     public class User
     {
         public Guid ID { get; set; } = Guid.NewGuid();
+       
         public string FirstName;
         public string LastName;
         public string FullName { get; set; }  
