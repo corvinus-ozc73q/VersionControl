0eabe2b6235e5539813b4ea39bad9c9474165158
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2022-10-07 11:33:41 +0200

    Exportalas2

--- task_week4/Form1.cs
+++ task_week4/Form1.cs
@@ -44,7 +44,42 @@ namespace task_week4
         "Alapterület (m2)",
         "Ár (mFt)",
         "Négyzetméter ár (Ft/m2)"};
+
+
+            xlSheet.Cells[1, 1] = headers[0];
+           
+            object[,] values = new object[Flats.Count, headers.Length];
+           
+            int counter = 0;
+            foreach (Flat f in Flats)
+            {
+                values[counter, 0] = f.Code;
+                // ...
+                values[counter, 8] = "";
+                counter++;
+
+                xlSheet.get_Range(
+            GetCell(2, 1),
+            GetCell(1 + values.GetLength(0), values.GetLength(1))).Value2 = values;
+            }
         }
+        private string GetCell(int x, int y)
+        {
+            string ExcelCoordinate = "";
+            int dividend = y;
+            int modulo;
+
+            while (dividend > 0)
+            {
+                modulo = (dividend - 1) % 26;
+                ExcelCoordinate = Convert.ToChar(65 + modulo).ToString() + ExcelCoordinate;
+                dividend = (int)((dividend - modulo) / 26);
+            }
+            ExcelCoordinate += x.ToString();
+
+            return ExcelCoordinate;
+        }
+
 
         private void CreateExcel()
         {
