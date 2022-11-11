88c5c28a8d0ef1d54d9ea362e0279ec2924d5552
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2022-11-11 12:25:43 +0100

    Portfolio

--- Week7/Form1.cs
+++ Week7/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week7.Entities;
 
 namespace Week7
 {
@@ -14,11 +15,21 @@ namespace Week7
     {
         PortfolioEntities context = new PortfolioEntities();
         List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
+
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
         }
 
         private void Form1_Load(object sender, EventArgs e)
