67525d9b97609bf76462aa5b85c9a732476af6ca
Gábor Ferenc Korbács <gabor.korbacs@stud.uni-corvinus.hu>
2022-12-02 10:26:09 +0100

    Classes

---
+++ Week_9/Entities/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_9.Entities
+{
+    public class Person
+    {
+        public int Birthyear { get; set; }
+        public Gender Gender { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person ()
+        {
+            IsAlive = true;
+        }
+
+    }
+}
